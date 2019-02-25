e932b72027debb7c4da31769c0997fe183b9377a
Frosina Marinoska <46903821+Frosina-Marinoska@users.noreply.github.com>
2019-02-25 14:23:02 +0100

    Add files via upload

---
+++ HW03/script.js
@@ -0,0 +1,6 @@
+let article = document.getElementsByClassName("HM03");
+console.log(article);
+article[0].innerHTML += `<h2> Naslov </h2>`;
+article[0].innerHTML += `<p> prv paragraf dodaden so js t.e.preku dinamicen kod </p>`;
+article[0].innerHTML += `<a href="#https://www.youtube.com/watch?v=iNOgLEhnWjw">...read more </a>`;
+
